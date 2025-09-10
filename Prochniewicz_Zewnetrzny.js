176342966b358102008eb615142401e42a76f97a
Lasis <sz.prochniewicz@gmail.com>
2025-09-10 20:19:39 +0200

    Update Prochniewicz_Zewnetrzny.js

--- Prochniewicz_Zewnetrzny.js
+++ Prochniewicz_Zewnetrzny.js
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-  <meta charset="UTF-8">
-  <title>Ostatnia Modyfikacja</title>
-</head>
-<body>
-  <script>
-    document.write('<p style="color:#FF00FF; font-size:xx-large; font-weight:bold; text-decoration:line-through;">Ostatnia Modyfikacja Strony</p>');
-    document.write('<p style="color:#FF00FF; font-size:xx-large; font-weight:bold; text-decoration:line-through;">' + document.lastModified + '</p>');
-  </script>
-</body>
-</html>
+document.write('<p style="color:#FF00FF; font-size:xx-large; font-weight:bold; text-decoration:line-through;">Ostatnia Modyfikacja Strony</p>'); document.write('<p style="color:#FF00FF; font-size:xx-large; font-weight:bold; text-decoration:line-through;">' + document.lastModified + '</p>');
